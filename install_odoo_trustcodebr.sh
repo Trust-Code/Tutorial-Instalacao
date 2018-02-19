6b7fa624b7f3bd9bdf3bf5249ef53d0b0aacb8d8
Silmar Pinheiro <pinheirosilmar@gmail.com>
2018-02-19 18:06:21 -0300

    adding module

--- install_odoo_trustcodebr.sh
+++ install_odoo_trustcodebr.sh
@@ -162,7 +162,46 @@ then
     source ve/bin/activate
     pip3 install --upgrade pip
     pip3 install --upgrade setuptools
-    pip3 install Babel==1.3
+    pip3 install Babel==2.3.4
+    pip3 install decorator==4.0.10
+    pip3 install docutils==0.12
+    pip3 install docutils==0.12
+    pip3 install ebaysdk==2.1.5
+    pip3 install feedparser==5.2.1
+    pip3 install gevent==1.1.2 ; sys_platform != 'win32'
+    pip3 install greenlet==0.4.10
+    pip3 install html2text==2016.9.19
+    pip3 install Jinja2==2.8
+    pip3 install lxml==3.7.1 ; sys_platform != 'win32'
+    pip3 install lxml ; sys_platform == 'win32'
+    pip3 install Mako==1.0.4
+    pip3 install MarkupSafe==0.23
+    pip3 install mock==2.0.0
+    pip3 install num2words==0.5.4
+    pip3 install ofxparse==0.16
+    pip3 install passlib==1.6.5
+    pip3 install Pillow==4.0.0
+    pip3 install psutil==4.3.1; sys_platform != 'win32'
+    pip3 install psycopg2==2.7.3.1; sys_platform != 'win32'
+    pip3 install pydot==1.2.3
+    pip3 install pyldap==2.4.28; sys_platform != 'win32'
+    pip3 install pyparsing==2.1.10
+    pip3 install PyPDF2==1.26.0
+    pip3 install pyserial==3.1.1
+    pip3 install python-dateutil==2.5.3
+    pip3 install pytz==2016.7
+    pip3 install pyusb==1.0.0
+    pip3 install PyYAML==3.12
+    pip3 install qrcode==5.3
+    pip3 install reportlab==3.3.0
+    pip3 install requests==2.11.1
+    pip3 install suds-jurko==0.6
+    pip3 install vatnumber==1.2
+    pip3 install vobject==0.9.3
+    pip3 install Werkzeug==0.11.15
+    pip3 install XlsxWriter==0.9.3
+    pip3 install xlwt==1.3.*
+    pip3 install xlrd==1.0.0
     pip3 install Jinja2==2.7.3
     pip3 install Mako==1.0.1
     pip3 install MarkupSafe==0.23
@@ -204,7 +243,9 @@ then
     pip3 install requests==2.6.0
     pip3 install six==1.9.0
     pip3 install suds-jurko==0.6
-    pip3 install vobject==0.9.5
+    pip3 install vobject
+    pip3 install qrcode
+    pip3 install pyldap
     #pip3 install wsgiref==0.1.2 Already included by default(python 3)
     pip3 install XlsxWriter==0.7.7
     pip3 install xlwt==1.3.0
@@ -234,7 +275,10 @@ echo ">>> pip e seus requerimentos estão instalados. <<<"
 
 echo "Clonando repositório oficial Odoo no GitHub. Isso pode demorar um bom tempo."
 echo "Se sua internet é lenta, recomenda-se tomar um café enquanto aguarda."
-git clone --depth 1 -b  $ODOO_VERSION.0 https://github.com/odoo/odoo.git ~/odoo
+git clone --depth 1 https://github.com/odoo/odoo.git ~/odoo
+cd ~/odoo
+git checkout $ODOO_VERSION.0
+
 
 echo "Terminando o arquivo de configuração, quase lá."
 rm ~/odoo/odoo-config
